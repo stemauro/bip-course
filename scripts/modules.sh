4e049b5e71ff4cb139778e580e4b9d410e41f3a1
maurogiovanni1 <s.maurogiovanni@fz-juelich.de>
2025-04-28 15:59:22 +0200

    :heavy_plus_sign: Add PIL

---
+++ scripts/modules.sh
@@ -0,0 +1,8 @@
+module purge
+module load Stages/2025 # any stage can be used
+module load GCC
+module load Python
+module load PyTorch
+module load torchvision
+module load Pillow-SIMD
+module load SciPy-Stack
