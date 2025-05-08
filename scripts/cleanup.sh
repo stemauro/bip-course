7dac53963233edea5c4a81b323911f6d0b2efde3
maurogiovanni1 <s.maurogiovanni@fz-juelich.de>
2025-05-08 16:56:02 +0200

    :poop: Tweak environment cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -4,8 +4,8 @@
 current_file="${BASH_SOURCE[0]:-${(%):-%x}}"
 scripts_dir="$(realpath "$(dirname "$current_file")")"
 
-# Deactivate current environment
-deactivate
+# Activate environment
+source "${scripts_dir}/activate.sh"
 
 # Remove the virtualenv folder and associated soft-links
 echo -e "🧹 Cleaning environment at: \033[;34m${VENV_DIR}/\033[0m"
@@ -14,5 +14,8 @@ rm --recursive "${VENV_DIR}"
 
 # Remove soft-link the virtualenv kernel
 unlink "${KERNEL_SPECS_DIR}/${KERNEL_NAME}"
+
+# Cleanup
+deactivate
 
 echo -e "\033[1;32m ✓\033[0m Environment '\033[1;37m${KERNEL_NAME}\033[0m' successfully removed."
