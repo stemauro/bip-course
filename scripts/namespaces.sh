ea66206c55620572db301bee335f694e8736b5bf
maurogiovanni1 <s.maurogiovanni@fz-juelich.de>
2025-05-14 10:08:41 +0200

    :wastebasket: Deprecate secrets file

--- scripts/namespaces.sh
+++ scripts/namespaces.sh
@@ -6,13 +6,11 @@ current_file="${BASH_SOURCE[0]:-${(%):-%x}}"
 scripts_dir="$(realpath "$(dirname "$current_file")")"
 export PROJECT_DIR="$(realpath "$(dirname "$scripts_dir")")"
 
-source "${scripts_dir}/secrets.sh"
-
 # Kernel
 export KERNEL_NAME="$(echo "${kernel_name}" | awk '{print tolower($0)}')"
 export KERNEL_SPECS_PREFIX="${HOME}/.local"
 export KERNEL_SPECS_DIR="${KERNEL_SPECS_PREFIX}/share/jupyter/kernels"
-export KERNEL_VENVS_DIR="${PROJECT}/${USER}/jupyter/kernels"
+export KERNEL_VENVS_DIR="${PROJECT_training2504}/${USER}/jupyter/kernels"
 
 # Virtual environment
 export VENV_DIR="${KERNEL_VENVS_DIR}/${KERNEL_NAME}"
